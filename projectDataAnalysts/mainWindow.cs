560760ff8bbad5208d1dc07f689307d3c3561564
antoydany <antonydany10@hotmail.com>
2024-04-14 02:19:03 -0700

    add dragDrop and modify dragEnter event to gridData

--- projectDataAnalysts/mainWindow.cs
+++ projectDataAnalysts/mainWindow.cs
@@ -25,13 +25,33 @@ namespace projectDataAnalysts
 
         }
 
+        private void ImportCsvDropGridData_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length == 1 && Path.GetExtension(files[0]).ToLower() == ".csv")
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+            }
+        }
+
+        //cargar svg arrastrando el archivo al DataGridView
+        private void ImportCsvDropGridData_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] sources = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            Console.WriteLine(sources[0]);
+            //activar cuando se suelta el objeto
+            WriteToGridAllData(sources[0]);
+        }
+
         private void ImportCsvBtn_Click(object sender, EventArgs e)
         {
             openFileDialogCsv = new OpenFileDialog
             {
                 Filter = "CSV files (*.csv)|*.csv"
             };
-
 
             if (openFileDialogCsv.ShowDialog() == DialogResult.OK)
             {
@@ -53,50 +73,38 @@ namespace projectDataAnalysts
         }
         private void WriteToGridAllData(string filename)
         {
-                    //base de datos para TODA la info de la tabla
-                    DataTable allDataTable = new DataTable();
+            //base de datos para TODA la info de la tabla
+            DataTable allDataTable = new DataTable();
 
 
-                    using (var sr = new StreamReader(openFileDialogCsv.FileName, Encoding.UTF8))
-                    {
-                        //encabezados de la tabla
-                        string[] headers = sr.ReadLine().Split(',');
+            using (var sr = new StreamReader(filename, Encoding.UTF8))
+            {
+                //encabezados de la tabla
+                string[] headers = sr.ReadLine().Split(',');
 
-                        foreach (string header in headers)
-                        {
-                            allDataTable.Columns.Add(header);
-                        }
-
-                        while (!sr.EndOfStream)
-                        {
-                            var line = sr.ReadLine();
-                            var values = line.Split(',');
-
-                            if(values.Length == headers.Length)
-                            {
-                                allDataTable.Rows.Add(values);
-                            }
-                            else { 
-
-                            Console.WriteLine('e');
-                            }
-                        } 
-                    }
-                    //pasa los valores al gridview
-                    gridAllData.DataSource = allDataTable;
-
-                }
-                catch (Exception ex)
+                foreach (string header in headers)
                 {
-                    // Handle any exceptions here
-                    MessageBox.Show($"Error reading file: {ex.Message}");
+                    allDataTable.Columns.Add(header);
+                }
+
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var values = line.Split(',');
+
+                    if (values.Length == headers.Length)
+                    {
+                        allDataTable.Rows.Add(values);
+                    }
+                    else
+                    {
+
+                        Console.WriteLine('e');
+                    }
                 }
             }
-        }
-
-        private void BtnCleanGridData_Click(object sender, EventArgs e)
-        {
-            gridAllData.DataSource = null;
+            //pasa los valores al gridview
+            gridAllData.DataSource = allDataTable;
         }
     }
 }
