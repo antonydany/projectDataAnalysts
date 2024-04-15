86fff003b303e6e5fc86722747d2ad73ad15c6f7
antoydany <antonydany10@hotmail.com>
2024-04-14 22:05:37 -0700

    modify design and style form

--- projectDataAnalysts/mainWindow.Designer.cs
+++ projectDataAnalysts/mainWindow.Designer.cs
@@ -30,6 +30,8 @@ namespace projectDataAnalysts
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.gridAllData = new System.Windows.Forms.DataGridView();
             this.importCsvBtn = new System.Windows.Forms.Button();
             this.btnCleanGridData = new System.Windows.Forms.Button();
@@ -40,60 +42,88 @@ namespace projectDataAnalysts
             // 
             // gridAllData
             // 
-            this.gridAllData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridAllData.AllowDrop = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(232)))));
+            this.gridAllData.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.gridAllData.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.gridAllData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gridAllData.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridAllData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.gridAllData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridAllData.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridAllData.GridColor = System.Drawing.SystemColors.AppWorkspace;
             this.gridAllData.Location = new System.Drawing.Point(12, 12);
             this.gridAllData.Name = "gridAllData";
-            this.gridAllData.Size = new System.Drawing.Size(776, 375);
+            this.gridAllData.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridAllData.Size = new System.Drawing.Size(792, 414);
             this.gridAllData.TabIndex = 0;
-            this.gridAllData.AllowDrop = true;
-            this.gridAllData.DragEnter += new DragEventHandler(this.ImportCsvDropGridData_DragEnter);
-            this.gridAllData.DragDrop += new DragEventHandler(this.ImportCsvDropGridData_DragDrop);
+            this.gridAllData.DragDrop += new System.Windows.Forms.DragEventHandler(this.ImportCsvDropGridData_DragDrop);
+            this.gridAllData.DragEnter += new System.Windows.Forms.DragEventHandler(this.ImportCsvDropGridData_DragEnter);
             // 
             // importCsvBtn
             // 
             this.importCsvBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.importCsvBtn.Location = new System.Drawing.Point(301, 3);
+            this.importCsvBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.importCsvBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.importCsvBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(167)))));
+            this.importCsvBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(89)))));
+            this.importCsvBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.importCsvBtn.ForeColor = System.Drawing.Color.White;
+            this.importCsvBtn.Location = new System.Drawing.Point(321, 9);
             this.importCsvBtn.Name = "importCsvBtn";
             this.importCsvBtn.Size = new System.Drawing.Size(105, 39);
             this.importCsvBtn.TabIndex = 1;
             this.importCsvBtn.Text = "Import CSV";
-            this.importCsvBtn.UseVisualStyleBackColor = true;
+            this.importCsvBtn.UseVisualStyleBackColor = false;
             this.importCsvBtn.Click += new System.EventHandler(this.ImportCsvBtn_Click);
             // 
             // btnCleanGridData
             // 
             this.btnCleanGridData.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCleanGridData.Location = new System.Drawing.Point(412, 3);
+            this.btnCleanGridData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.btnCleanGridData.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCleanGridData.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(167)))));
+            this.btnCleanGridData.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(89)))));
+            this.btnCleanGridData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCleanGridData.ForeColor = System.Drawing.Color.White;
+            this.btnCleanGridData.Location = new System.Drawing.Point(432, 9);
             this.btnCleanGridData.Name = "btnCleanGridData";
             this.btnCleanGridData.Size = new System.Drawing.Size(105, 39);
             this.btnCleanGridData.TabIndex = 2;
             this.btnCleanGridData.Text = "Limpiar";
-            this.btnCleanGridData.UseVisualStyleBackColor = true;
+            this.btnCleanGridData.UseVisualStyleBackColor = false;
             this.btnCleanGridData.Click += new System.EventHandler(this.BtnCleanGridData_Click);
             // 
             // panelMainButtons
             // 
             this.panelMainButtons.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
-            this.panelMainButtons.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.panelMainButtons.Controls.Add(this.importCsvBtn);
             this.panelMainButtons.Controls.Add(this.btnCleanGridData);
-            this.panelMainButtons.Location = new System.Drawing.Point(12, 393);
+            this.panelMainButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMainButtons.Location = new System.Drawing.Point(0, 432);
             this.panelMainButtons.Name = "panelMainButtons";
-            this.panelMainButtons.Size = new System.Drawing.Size(776, 45);
+            this.panelMainButtons.Size = new System.Drawing.Size(816, 57);
             this.panelMainButtons.TabIndex = 3;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(816, 489);
             this.Controls.Add(this.panelMainButtons);
             this.Controls.Add(this.gridAllData);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "MainWindow";
             this.Text = "projectDataAnalyst";
